81a169e2dacb1879cb60e30e7d1040b1ca404c83
Alekh Jindal <alekhjindal@gmail.com>
2015-06-14 21:13:49 -0400

    settings file

--- test/core/index/Settings.java
+++ test/core/index/Settings.java
@@ -2,8 +2,13 @@ package core.index;
 
 
 public class Settings {
-	public static String tpchPath = "/Users/anil/Dev/repos/tpch-dbgen/";
-	public static String localPartitionDir = "/tmp";
-	public static String hdfsPartitionDir = "/user/anil/dodo";
-	public static String cartilageConf = "/Users/anil/Dev/repos/mdindex/conf/cartilage.properties";
+	public static String tpchPath = "/data/mdindex/tpch-dbgen/lineitem1000";
+	public static String localPartitionDir = "/Users/alekh/Work/Cartilage/tmp";
+	public static String hdfsPartitionDir = "/user/anil/data/";
+	public static String cartilageConf = "/home/mdindex/cartilage.properties";
+	
+//	public static String tpchPath = "/Users/alekh/Work/Cartilage/support/datasets/scale_1/";
+//	public static String localPartitionDir = "/Users/alekh/Work/Cartilage/tmp";
+//	public static String hdfsPartitionDir = "/user/alekh/dodo/";
+//	public static String cartilageConf = "/Users/alekh/Work/Cartilage/MDIndex/conf/cartilage.properties";
 }
