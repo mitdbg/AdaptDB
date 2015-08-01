3db2f9e72e86ef56f99f99b7952a9090a2067ee6
Alekh Jindal <alekhjindal@gmail.com>
2015-08-01 01:10:24 -0400

    more pieces in the algo; the things that are left are marked as TODO

--- src/main/java/core/access/spark/join/algo/HyperJoinTuned.java
+++ src/main/java/core/access/spark/join/algo/HyperJoinTuned.java
@@ -1,13 +1,19 @@
 package core.access.spark.join.algo;
 
 import java.util.List;
+import java.util.Set;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputSplit;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
+import core.access.iterator.ReusablePartitionIterator;
+import core.access.spark.SparkInputFormat.SparkFileSplit;
 import core.access.spark.join.HPJoinInput;
 import core.index.MDIndex;
+import core.utils.Pair;
 import core.utils.Range;
 
 /**
@@ -31,7 +37,10 @@ public class HyperJoinTuned extends JoinAlgo {
 
 	private HPJoinInput joinInput1;
 	private HPJoinInput joinInput2;	
+	
 	private int minSplits;
+	private double maxPartitionSize;
+	private double maxHashTableSize;
 	
 	public HyperJoinTuned(HPJoinInput joinInput1, HPJoinInput joinInput2, int minSplits) {
 		this.joinInput1 = joinInput1;
@@ -80,8 +89,23 @@ public class HyperJoinTuned extends JoinAlgo {
 	 * @param partitionSet
 	 * @return
 	 */
-	protected PartitionSet iterate(PartitionSet partitionSet){
-		return null;	// TODO
+	protected void iterate(PartitionSet partitionSet){
+		// create a list of all combine-able partition pairs 
+		List<Pair<Partition,Partition>> candidatePairs = Lists.newArrayList();
+		for(Partition p1: partitionSet.getPartitions())
+			for(Partition p2: partitionSet.getPartitions())
+				if(p1!=p2 && p1.isCombineCandidate() && p2.isCombineCandidate())
+					candidatePairs.add(new Pair<Partition,Partition>(p1, p2));
+		
+		// TODO: sort the partition pairs based on how much they can reduce the cost
+
+		// combine the first pair which succeeds 
+		for(Pair<Partition,Partition> p: candidatePairs){
+			if(p.first.tryCombine(p.second, maxPartitionSize, maxHashTableSize)){
+				partitionSet.remove(p.second);
+				break;
+			}
+		}
 	}
 	
 	/**
@@ -96,10 +120,10 @@ public class HyperJoinTuned extends JoinAlgo {
 		List<VBucket> vbuckets = getVirtualBuckets(joinInput1.getBucketRanges(), rangelets);
 		
 		// step 3: initialize the partition set
-		PartitionSet partitionSet = new PartitionSet(vbuckets);
+		PartitionSet partitionSet = new PartitionSet(vbuckets, joinInput2);
 		
 		// step 4: the heuristic based combine step
-		double initialC = partitionSet.C();
+		long initialC = partitionSet.C();
 		do{
 			iterate(partitionSet);
 		} while(
@@ -124,20 +148,42 @@ public class HyperJoinTuned extends JoinAlgo {
 
 	
 	/**
-	 * The virtual bucket instance. 
-	 * 
-	 * @author alekh
-	 */
-	public static class VBucket{
-	}
-	
-	/**
 	 * The physical bucket instance.
 	 * I guess this cane be replaced with one of the existing classes, e.g. MDIndex.BucketInfo ?
 	 * 
 	 * @author alekh
 	 */
 	public static class PBucket{
+		private long size;
+		private Path path;
+		public PBucket(MDIndex.BucketInfo info){
+			// TODO: extract bucket size and path from info
+		}
+		public long size(){
+			return size;
+		}
+		public Path path(){
+			return path;
+		}
+	}
+	
+	/**
+	 * The virtual bucket instance. 
+	 * 
+	 * @author alekh
+	 */
+	public static class VBucket{
+		private PBucket b;
+		private Range r;
+		public VBucket(PBucket b, Range r){
+			this.b = b;
+		}
+		public PBucket b(){
+			return b;
+		}
+		public Range range(){
+			return r;
+		}
 	}
 	
 	/**
@@ -152,31 +198,65 @@ public class HyperJoinTuned extends JoinAlgo {
 		 * - the hash table size, i.e. sB, is less than the max size 
 		 */
 		private boolean combineCandidate = true;
+		private Set<VBucket> vbuckets;
+		private Set<PBucket> pbuckets;
+		private Range range;
+		private long sizeA, sizeB;
 		
-		public Partition(VBucket vbucket){
-			// TODO
+		public Partition(VBucket vbucket, HPJoinInput secondInput){
+			vbuckets = Sets.newHashSet(vbucket);
+			pbuckets = Sets.newHashSet(vbucket.b());
+			range = vbucket.range().clone();	// TODO: make sure clone is implemented in range 
+												// (we use clone because the range of this partition could be later extended)
+			sizeA = vbucket.b().size();
+			sizeB = lookupSizeB(range);
 		}
-		public List<VBucket> v(){
-			return null;
+		private long lookupSizeB(Range r){
+			// TODO: need to do index lookup from the second input
+			return 0;
 		}
-		public List<PBucket> b(){
-			return null;
+		public Set<VBucket> v(){
+			return vbuckets;
+		}
+		public Set<PBucket> b(){
+			return pbuckets;
 		}
 		public Range r(){
-			return null;
+			return range;
 		}
-		public double sA(){
-			return 0;
+		public long sA(){
+			return sizeA;
 		}
-		public double sB(){
-			return 0;
+		public long sB(){
+			return sizeB;
 		}
-		public double C(){
-			return 0;
+		public long C(){
+			return sA() + sB();
 		}
-		public Partition combine(){
-			return null;
-			// TODO: update the combineCandidate flag accordingly
+		/**
+		 * Try to combine this partition with a new one.
+		 * 
+		 * @param p -- the new partition to be combined with.
+		 * @return -- true, if the combine was successful; false, otherwise.
+		 */
+		public boolean tryCombine(Partition p, double maxPartitionSize, double maxHashTableSize){
+			// check max partition size
+			combineCandidate &= (sizeA + p.sizeA) <= maxPartitionSize;
+			
+			// check max hash table size
+			Range tmp = range.clone();	// TODO: again, verify that Range supports clone
+			tmp.union(p.range);
+			combineCandidate &= lookupSizeB(tmp) <= maxHashTableSize;
+			
+			if(combineCandidate){				
+				vbuckets.addAll(p.vbuckets);
+				pbuckets.addAll(p.pbuckets);
+				range.union(p.range);
+				sizeA += p.sizeA;
+				sizeB = lookupSizeB(range);
+			}
+
+			return combineCandidate;
 		}
 		public boolean isCombineCandidate(){
 			return combineCandidate;					
@@ -189,11 +269,11 @@ public class HyperJoinTuned extends JoinAlgo {
 	 * @author alekh
 	 */
 	public static class PartitionSet {
-		private List<Partition> partitions;		
-		public PartitionSet(List<VBucket> vbuckets){
-			partitions = Lists.newArrayList();
+		private Set<Partition> partitions;		
+		public PartitionSet(List<VBucket> vbuckets, HPJoinInput secondInput){
+			partitions = Sets.newHashSet();
 			for(VBucket vbucket: vbuckets)
-				partitions.add(new Partition(vbucket));
+				partitions.add(new Partition(vbucket, secondInput));
 		}
 		public void add(Partition g){
 			partitions.add(g);
@@ -204,8 +284,8 @@ public class HyperJoinTuned extends JoinAlgo {
 		public int size(){
 			return partitions.size();
 		}
-		public double C(){
-			double c = 0;
+		public long C(){
+			long c = 0;
 			for(Partition p: partitions)
 				c += p.C();
 			return c;
@@ -216,8 +296,25 @@ public class HyperJoinTuned extends JoinAlgo {
 				c += p.combineCandidate ? 1:0;
 			return c;
 		}
+		public Set<Partition> getPartitions(){
+			return partitions;
+		}
 		public List<InputSplit> getInputSplits(){
-			return null;
+			List<InputSplit> finalSplits = Lists.newArrayList();
+			for(Partition partition: partitions){
+				Path[] paths =new Path[partition.b().size()];
+				long[] lengths =new long[partition.b().size()];
+				int i=0;
+				for(PBucket pbucket: partition.b()){
+					paths[i] = pbucket.path();
+					lengths[i] = pbucket.size();
+					i++;
+				}
+				finalSplits.add(
+						new SparkFileSplit(paths, lengths, new ReusablePartitionIterator())
+					);
+			}			
+			return finalSplits;
 		}
 	}
 }
