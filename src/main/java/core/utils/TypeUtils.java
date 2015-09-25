085e8fd9b0b82845d99e5cd83d4e5f1113e13fe6
Anil Shanbhag <anilashanbhag@gmail.com>
2015-09-25 16:02:31 -0400

    making one day less work correctly

--- src/main/java/core/utils/TypeUtils.java
+++ src/main/java/core/utils/TypeUtils.java
@@ -1,8 +1,8 @@
 package core.utils;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.GregorianCalendar;
 
 public class TypeUtils {
 	public static enum TYPE {
@@ -44,24 +44,16 @@ public class TypeUtils {
 			this.day = day;
 		}
 
+		// Though expensive to create the calendar and find out, it works.
+		// Infrequent operation, not worth optimizing.
 		public SimpleDate oneDayLess() {
-			int nYear = year, nMonth = month, nDay = day;
-			if (day > 1)
-				nDay--;
-			else {
-				if (month > 1) {
-					nMonth--;
-					nDay = daysPerMonth[nMonth - 1];
-				} else {
-					nMonth = 12;
-					nYear--;
-					nDay = daysPerMonth[nMonth - 1];
-				}
-			}
-
-			return new SimpleDate(nYear, nMonth, nDay);
+			Calendar c = new GregorianCalendar(year, month, day);
+			c.add(Calendar.DAY_OF_MONTH, -1);
+			return new SimpleDate(c.get(Calendar.YEAR), c.get(Calendar.MONTH),
+					c.get(Calendar.DAY_OF_MONTH));
 		}
 
+		@Override
 		public int compareTo(SimpleDate d) {
 			if (d.getYear() < year
 					|| (d.getYear() == year && d.getMonth() < month)
@@ -87,52 +79,6 @@ public class TypeUtils {
 			ret += (month < 10 ? "0" + month : month) + "-";
 			ret += (day < 10 ? "0" + day : day);
 			return ret;
-		}
-	}
-
-	public static boolean isInt(String s) {
-		try {
-			Integer.parseInt(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	public static boolean isLong(String s) {
-		try {
-			Long.parseLong(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	public static boolean isFloat(String s) {
-		try {
-			Float.parseFloat(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	public static boolean isDouble(String s) {
-		try {
-			Double.parseDouble(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
-
-	// dateFormat e.g. "yyyy-MM-dd"
-	public static boolean isDate(String s, SimpleDateFormat sdf) {
-		try {
-			sdf.parse(s);
-			return true;
-		} catch (ParseException e) {
-			return false;
 		}
 	}
 
@@ -163,13 +109,6 @@ public class TypeUtils {
 			return ((SimpleDate) x).compareTo((SimpleDate) y);
 		case STRING:
 			return ((String) x).compareTo((String) y);
-		case VARCHAR:
-			if (x.hashCode() > y.hashCode())
-				return 1;
-			else if (x.hashCode() < y.hashCode())
-				return -1;
-			else
-				return 0;
 		default:
 			System.err.println("Unknown TYPE in compareTo");
 			return 0;
@@ -181,30 +120,35 @@ public class TypeUtils {
 		switch (type) {
 		case INT:
 			return new Comparator<Object>() {
+				@Override
 				public int compare(Object o1, Object o2) {
 					return ((Integer) o1).compareTo((Integer) o2);
 				}
 			};
 		case LONG:
 			return new Comparator<Object>() {
+				@Override
 				public int compare(Object o1, Object o2) {
 					return ((Long) o1).compareTo((Long) o2);
 				}
 			};
 		case DOUBLE:
 			return new Comparator<Object>() {
+				@Override
 				public int compare(Object o1, Object o2) {
 					return ((Float) o1).compareTo((Float) o2);
 				}
 			};
 		case DATE:
 			return new Comparator<Object>() {
+				@Override
 				public int compare(Object o1, Object o2) {
 					return ((SimpleDate) o1).compareTo((SimpleDate) o2);
 				}
 			};
 		case STRING:
 			return new Comparator<Object>() {
+				@Override
 				public int compare(Object o1, Object o2) {
 					return ((String) o1).compareTo((String) o2);
 				}
