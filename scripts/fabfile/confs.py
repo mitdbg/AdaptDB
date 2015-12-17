86b65c6afe8bb377a0d387c109eb4386778af68c
Yi Lu <luyi0619@outlook.com>
2015-12-16 21:28:07 -0500

    Drop comment from TPCH. ADD s_region and c_region

--- scripts/fabfile/confs.py
+++ scripts/fabfile/confs.py
@@ -36,7 +36,7 @@ confs = {
             "local_lineitem" : {
                 'JAR' : '/Users/ylu/Documents/workspace/mdindex/build/libs/mdindex-all.jar',
                 'CONF' : '/Users/ylu/Documents/workspace/mdindex/conf/ylu.properties',
-                'INPUTSDIR' : '/Users/ylu/Documents/workspace/mdindex/data/lineitem',
+                'INPUTSDIR' : '/Users/ylu/Documents/workspace/tpchdata/lineitem',
                 'TABLENAME' : 'lineitem',
                 'HDFSDIR' : '/user/ylu/lineitem',
                 'HOMEDIR' : '/Users/ylu',
@@ -45,8 +45,8 @@ confs = {
                 'DELIMITER': '|',
                 'NUMBUCKETS' : '16',
                 'NUMTUPLES': '6001215',
-                'NUMFIELDS' : '16',
-                'SCHEMA': 'l_orderkey int, l_partkey int, l_suppkey int, l_linenumber int, l_quantity double, l_extendedprice double, l_discount double, l_tax double, l_returnflag string,  l_linestatus string, l_shipdate date, l_commitdate date, l_receiptdate date, l_shipinstruct string, l_shipmode string, l_comment string',
+                'NUMFIELDS' : '15',
+                'SCHEMA': 'l_orderkey int, l_partkey int, l_suppkey int, l_linenumber int, l_quantity double, l_extendedprice double, l_discount double, l_tax double, l_returnflag string,  l_linestatus string, l_shipdate date, l_commitdate date, l_receiptdate date, l_shipinstruct string, l_shipmode string',
                 'USER': 'ylu',
                 'HOSTS': ['localhost'],
                 'ROLEDEFS': { 'master': ['localhost'] }
@@ -54,7 +54,7 @@ confs = {
             "local_orders" : {
                 'JAR' : '/Users/ylu/Documents/workspace/mdindex/build/libs/mdindex-all.jar',
                 'CONF' : '/Users/ylu/Documents/workspace/mdindex/conf/ylu.properties',
-                'INPUTSDIR' : '/Users/ylu/Documents/workspace/mdindex/data/orders',
+                'INPUTSDIR' : '/Users/ylu/Documents/workspace/tpchdata/orders',
                 'TABLENAME' : 'orders',
                 'HDFSDIR' : '/user/ylu/orders',
                 'HOMEDIR' : '/Users/ylu',
@@ -63,8 +63,8 @@ confs = {
                 'DELIMITER': '|',
                 'NUMBUCKETS' : '16',
                 'NUMTUPLES': '1500000',
-                'NUMFIELDS' : '9',
-                'SCHEMA': 'o_orderkey int, o_custkey int, o_orderstatus string, o_totalprice double, o_orderdate date, o_orderpriority string, o_clerk string, o_shippriority int, o_comment string',
+                'NUMFIELDS' : '8',
+                'SCHEMA': 'o_orderkey int, o_custkey int, o_orderstatus string, o_totalprice double, o_orderdate date, o_orderpriority string, o_clerk string, o_shippriority int',
                 'USER': 'ylu',
                 'HOSTS': ['localhost'],
                 'ROLEDEFS': { 'master': ['localhost'] }
@@ -72,7 +72,7 @@ confs = {
             "local_part" : {
                 'JAR' : '/Users/ylu/Documents/workspace/mdindex/build/libs/mdindex-all.jar',
                 'CONF' : '/Users/ylu/Documents/workspace/mdindex/conf/ylu.properties',
-                'INPUTSDIR' : '/Users/ylu/Documents/workspace/mdindex/data/part',
+                'INPUTSDIR' : '/Users/ylu/Documents/workspace/tpchdata/part',
                 'TABLENAME' : 'part',
                 'HDFSDIR' : '/user/ylu/part',
                 'HOMEDIR' : '/Users/ylu',
@@ -81,8 +81,8 @@ confs = {
                 'DELIMITER': '|',
                 'NUMBUCKETS' : '16',
                 'NUMTUPLES': '200000',
-                'NUMFIELDS' : '9',
-                'SCHEMA': 'p_partkey int, p_name string, p_mfgr string, p_brand string, p_type string, p_size int, p_container string, p_retailprice double, p_comment string',
+                'NUMFIELDS' : '8',
+                'SCHEMA': 'p_partkey int, p_name string, p_mfgr string, p_brand string, p_type string, p_size int, p_container string, p_retailprice double',
                 'USER': 'ylu',
                 'HOSTS': ['localhost'],
                 'ROLEDEFS': { 'master': ['localhost'] }
@@ -90,7 +90,7 @@ confs = {
              "local_supplier" : {
                 'JAR' : '/Users/ylu/Documents/workspace/mdindex/build/libs/mdindex-all.jar',
                 'CONF' : '/Users/ylu/Documents/workspace/mdindex/conf/ylu.properties',
-                'INPUTSDIR' : '/Users/ylu/Documents/workspace/mdindex/data/supplier',
+                'INPUTSDIR' : '/Users/ylu/Documents/workspace/tpchdata/supplier',
                 'TABLENAME' : 'supplier',
                 'HDFSDIR' : '/user/ylu/supplier',
                 'HOMEDIR' : '/Users/ylu',
@@ -100,7 +100,7 @@ confs = {
                 'NUMBUCKETS' : '16',
                 'NUMTUPLES': '10000',
                 'NUMFIELDS' : '7',
-                'SCHEMA': 's_suppkey int, s_name string, s_address string, s_nationkey int, s_phone string, s_acctbal double, s_comment string',
+                'SCHEMA': 's_suppkey int, s_name string, s_address string, s_phone string, s_acctbal double, s_nation string, s_region string',
                 'USER': 'ylu',
                 'HOSTS': ['localhost'],
                 'ROLEDEFS': { 'master': ['localhost'] }
@@ -108,7 +108,7 @@ confs = {
              "local_customer" : {
                 'JAR' : '/Users/ylu/Documents/workspace/mdindex/build/libs/mdindex-all.jar',
                 'CONF' : '/Users/ylu/Documents/workspace/mdindex/conf/ylu.properties',
-                'INPUTSDIR' : '/Users/ylu/Documents/workspace/mdindex/data/customer',
+                'INPUTSDIR' : '/Users/ylu/Documents/workspace/tpchdata/customer',
                 'TABLENAME' : 'customer',
                 'HDFSDIR' : '/user/ylu/customer',
                 'HOMEDIR' : '/Users/ylu',
@@ -118,7 +118,7 @@ confs = {
                 'NUMBUCKETS' : '16',
                 'NUMTUPLES': '150000',
                 'NUMFIELDS' : '8',
-                'SCHEMA': 'c_custkey int, c_name string, c_address string, c_nationkey int, c_phone string, c_acctbal double, c_mktsegment string , c_comment string',
+                'SCHEMA': 'c_custkey int, c_name string, c_address string, c_phone string, c_acctbal double, c_mktsegment string, c_nation string, c_region string',
                 'USER': 'ylu',
                 'HOSTS': ['localhost'],
                 'ROLEDEFS': { 'master': ['localhost'] }
--- src/test/scala/tpch_join.scala
+++ src/test/scala/tpch_join.scala
@@ -11,29 +11,105 @@ import org.apache.spark.sql.SaveMode
 //val PATH = "hdfs://istc2.csail.mit.edu:9000/user/ylu"
 
 val PATH = "hdfs://localhost:9000/raw"
+val DEST = "hdfs://localhost:9000/tpch"
 
-// Create order table.
-sqlContext.sql(s"""CREATE TEMPORARY TABLE orders (o_orderkey int, o_custkey int,
+
+// Create nation table.
+sqlContext.sql(s"""CREATE TEMPORARY TABLE nation (n_nationkey int, n_name string, n_regionkey int,
+	n_comment string)
+USING com.databricks.spark.csv
+OPTIONS (path "$PATH/nation.tbl", header "false", delimiter "|")""")
+
+// Create region table.
+sqlContext.sql(s"""CREATE TEMPORARY TABLE region (r_regionkey int, r_name string, r_comment string)
+USING com.databricks.spark.csv
+OPTIONS (path "$PATH/region.tbl", header "false", delimiter "|")""")
+
+
+
+// Create raw order table.
+sqlContext.sql(s"""CREATE TEMPORARY TABLE raworders (o_orderkey int, o_custkey int,
   o_orderstatus string, o_totalprice double, o_orderdate string, o_orderpriority string, o_clerk string,
   o_shippriority int, o_comment string)
 USING com.databricks.spark.csv
 OPTIONS (path "$PATH/orders.tbl", header "false", delimiter "|")""")
 
-// Create lineitem table.
-sqlContext.sql(s"""CREATE TEMPORARY TABLE lineitem (l_orderkey int, l_partkey int, l_suppkey int,
+val orders = sqlContext.sql(s"""SELECT o_orderkey, o_custkey, o_orderstatus,
+o_totalprice, o_orderdate, o_orderpriority, o_clerk, o_shippriority
+FROM raworders""")
+
+orders.registerTempTable("orders")
+orders.save("com.databricks.spark.csv", SaveMode.ErrorIfExists, Map("path" ->  (DEST + "/orders"), "delimiter" -> "|"))
+
+
+// Create raw lineitem table.
+sqlContext.sql(s"""CREATE TEMPORARY TABLE rawlineitem (l_orderkey int, l_partkey int, l_suppkey int,
 	l_linenumber int, l_quantity double, l_extendedprice double, l_discount double, l_tax double,
 	l_returnflag string,  l_linestatus string, l_shipdate string, l_commitdate string, l_receiptdate string,
 	l_shipinstruct string, l_shipmode string, l_comment string)
 USING com.databricks.spark.csv
 OPTIONS (path "$PATH/lineitem.tbl", header "false", delimiter "|")""")
 
+val lineitem = sqlContext.sql(s"""SELECT l_orderkey, l_partkey, l_suppkey,
+l_linenumber, l_quantity, l_extendedprice, l_discount, l_tax,
+l_returnflag, l_linestatus, l_shipdate, l_commitdate, l_receiptdate,
+l_shipinstruct, l_shipmode
+FROM rawlineitem""")
 
-// create customer table
+lineitem.registerTempTable("orders")
+lineitem.save("com.databricks.spark.csv", SaveMode.ErrorIfExists, Map("path" -> (DEST + "/lineitem"), "delimiter" -> "|"))
 
-sqlContext.sql(s"""CREATE TEMPORARY TABLE customer (c_custkey int, c_name string, c_address string,
+
+// Create raw customer table
+
+sqlContext.sql(s"""CREATE TEMPORARY TABLE rawcustomer (c_custkey int, c_name string, c_address string,
   c_nationkey int, c_phone string, c_acctbal double, c_mktsegment string , c_comment string)
 USING com.databricks.spark.csv
 OPTIONS (path "$PATH/customer.tbl", header "false", delimiter "|")""")
+
+
+val customer = sqlContext.sql(s"""SELECT c_custkey, c_name, c_address, c_phone, c_acctbal,
+  c_mktsegment, n_name as c_nation, r_name as c_region
+FROM
+	rawcustomer JOIN nation ON c_nationkey = n_nationkey
+	JOIN region ON n_regionkey = r_regionkey""")
+
+customer.registerTempTable("customer")
+customer.save("com.databricks.spark.csv", SaveMode.ErrorIfExists, Map("path" -> (DEST + "/customer"), "delimiter" -> "|"))
+
+
+
+// Create raw part table.
+
+sqlContext.sql(s"""CREATE TEMPORARY TABLE rawpart (p_partkey int, p_name string, p_mfgr string, p_brand string,
+	p_type string, p_size int, p_container string, p_retailprice double, p_comment string)
+USING com.databricks.spark.csv
+OPTIONS (path "$PATH/part.tbl", header "false", delimiter "|")""")
+
+val part = sqlContext.sql(s"""SELECT p_partkey, p_name, p_mfgr, p_brand,
+p_type, p_size, p_container, p_retailprice
+FROM rawpart""")
+
+part.registerTempTable("orders")
+part.save("com.databricks.spark.csv", SaveMode.ErrorIfExists, Map("path" -> (DEST + "/part"), "delimiter" -> "|"))
+
+
+
+// Create raw supplier table.
+
+sqlContext.sql(s"""CREATE TEMPORARY TABLE rawsupplier (s_suppkey int, s_name string, s_address string,
+	s_nationkey int, s_phone string, s_acctbal double, s_comment string)
+USING com.databricks.spark.csv
+OPTIONS (path "$PATH/supplier.tbl", header "false", delimiter "|")""")
+
+val supplier = sqlContext.sql(s"""SELECT s_suppkey, s_name, s_address, s_phone, s_acctbal, n_name AS s_nation, r_name AS s_region
+FROM
+	rawsupplier JOIN nation ON s_nationkey = n_nationkey
+	JOIN region ON n_regionkey = r_regionkey""")
+
+supplier.registerTempTable("supplier")
+supplier.save("com.databricks.spark.csv", SaveMode.ErrorIfExists, Map("path" -> (DEST + "/supplier"), "delimiter" -> "|"))
+
 
 // tpch3
 
