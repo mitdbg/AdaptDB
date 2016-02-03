74345df507152c19e2203f22ab74aed487648623
Anil Shanbhag <anils@mit.edu>
2016-02-03 09:04:05 -0500

    making sure we check the total tuple count at the end of index building

--- src/test/java/core/upfront/build/IndexBuilderTest.java
+++ src/test/java/core/upfront/build/IndexBuilderTest.java
@@ -10,6 +10,7 @@ import perf.benchmark.CreateTableInfo;
 import perf.benchmark.RunIndexBuilder;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by anil on 12/18/15.
@@ -25,16 +26,20 @@ public class IndexBuilderTest extends TestCase {
     }
 
     public void testEndToEnd() {
-        String homeDir = "/Users/anil/Dev/repos/mdindex";
+        String HOME_DIR = "/Users/anil/Dev/repos/mdindex";
+        Integer NUM_BUCKETS = 16;
+        Integer NUM_TUPLES = 30329;
+        String TABLE_NAME = "tpch";
+
         String[] args = new String[]{
-                "--tableName", "tpch",
-                "--numTuples", "30329",
+                "--tableName", TABLE_NAME,
+                "--numTuples", NUM_TUPLES.toString(),
                 "--delimiter", "|",
                 "--schema", "l_linenumber int, l_quantity double, l_extendedprice double, l_discount double, l_tax double, l_returnflag string,  l_linestatus string, l_shipdate date, l_commitdate date, l_receiptdate date, l_shipinstruct string, l_shipmode string, o_orderstatus string, o_totalprice double, o_orderdate date, o_orderpriority string, o_clerk string, o_shippriority int, p_name string, p_mfgr string, p_brand string, p_type string, p_size int, p_container string, p_retailprice double, s_name string, s_address string, s_phone string, s_acctbal double, s_nation string, s_region string, c_name string, c_address string, c_phone string, c_acctbal double, c_mktsegment string, c_nation string, c_region string",
-                "--inputsDir", homeDir + "/data/tpchd/",
+                "--inputsDir", HOME_DIR + "/data/tpchd/",
                 "--samplingRate", "0.1",
-                "--numBuckets", "16",
-                "--conf", homeDir + "/conf/test.properties"
+                "--numBuckets", NUM_BUCKETS.toString(),
+                "--conf", HOME_DIR + "/conf/test.properties"
         };
         BenchmarkSettings.loadSettings(args);
 
@@ -54,6 +59,7 @@ public class IndexBuilderTest extends TestCase {
         t.buildRobustTreeFromSamples();
         t.writePartitionsFromIndex();
 
+        // Make sure the necessary files have been created.
         String basePath = cfg.getHDFS_WORKING_DIR();
         try {
             assertTrue(fs.exists(new Path(basePath + "/tpch/info")));
@@ -65,5 +71,14 @@ public class IndexBuilderTest extends TestCase {
         } catch(IOException e) {
             fail("fs.exists call failed");
         }
+
+        // Make sure no tuples were lost.
+        int total = 0;
+        for (int i=0; i<16; i++) {
+            List<String> lines = HDFSUtils.readHDFSLines(fs,
+                    cfg.getHDFS_WORKING_DIR() + "/" + TABLE_NAME + "/data/" + i);
+            total += lines.size();
+        }
+        assertTrue(total == NUM_TUPLES);
     }
 }
