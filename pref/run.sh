53d55cc2cf06842c9df8eb6e7d4f9bcbe6081df7
Yi Lu <luyi0619@gmail.com>
2016-09-26 11:58:35 -0400

    update pref run.sh

--- pref/run.sh
+++ pref/run.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch3
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch5
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch6
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch8
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch10
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch12
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch14
-# ~/Documents/workspace/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 6G --executor-cores 2 --master "local[2]" target/scala-2.10/pref_workload-assembly-1.0.jar tpch19
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar distinct
 
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch3
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch5
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch6
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch8
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch10
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch12
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch14
-~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 1 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch19
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch3
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch5
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch6
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch8
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch10
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch12
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch14
+~/spark-1.6.0-bin-hadoop2.6/bin/spark-submit --class pref_workload --executor-memory 200G --executor-cores 4 --master spark://istc13.csail.mit.edu:7077 ~/AdaptDB/pref/target/scala-2.10/pref_workload-assembly-1.0.jar tpch19
